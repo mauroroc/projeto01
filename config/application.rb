509235fcd27db6e11c8f803413f3ad43ec3c8c2f
Mauro Rocha <mauror@mauror.com.br>
2021-02-27 19:29:45 -0300

    WIP - Débito Técnico e I18n OK

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Projeto01
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
+    config.i18n.default_locale = :'pt-BR'
     #config.serve_static_assets = true
     # Configuration for the application, engines, and railties goes here.
     #
