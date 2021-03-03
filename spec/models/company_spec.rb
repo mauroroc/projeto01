aa276a096f5de8b081eb349cb08efd8d5f35749c
Mauro Rocha <mauror@mauror.com.br>
2021-03-03 18:46:22 -0300

    WIP Feedback - Testes - Shoulda_Matchers

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Company, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it{ is_expected.to have_many(:employee) }
+    it{ is_expected.to have_many(:job) }
+  end
 end
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Job, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { should belong_to(:company).class_name('Company') }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:need) }
+    it { should validate_presence_of(:expiration) }
+    it { should validate_presence_of(:quantity) }
+    it { should validate_presence_of(:description) }    
+  end
+
 end
